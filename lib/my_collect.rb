0045c61c6151d191a58f98c381ccfc3588f33649
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 03:11:02 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect array
   holder = []
   if array.size == 0
     return array
-  elsif array.size != 0
+  elsif array.size > 0
         ounter = 0
   while counter < array.size
         array.unshift = holder
