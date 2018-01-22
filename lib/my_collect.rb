6b47c5fc61a2be1d417fd8d29236e9be4f73fc87
Ben Matare <ben.a.matare@gmail.com>
2018-01-22 03:09:19 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,12 @@
 
 def my_collect array
   holder = []
-  if array.size = 0
+    if array.size = 0
     return array
-elsif if array.size != 0
-counter = 0
-  while counter < array.size
-    array.upcase = holder
-end
-return holder
-end
-
+      elsif array.size != 0
+            counter = 0
+              while counter < array.size
+                    array.upcase = holder
+                  end
+          return holder
 end
